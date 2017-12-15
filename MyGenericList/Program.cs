27fdf0c4ca2835f7d0006fb6ac486617b89b53dd
MarcinB <marcio_br@interia.pl>
2017-12-15 16:17:22 +0100

    MyGenericList work like List. Only read and write.

--- MyGenericList/Program.cs
+++ MyGenericList/Program.cs
@@ -6,10 +6,70 @@ using System.Threading.Tasks;
 
 namespace MyGenericList
 {
+    public class MyGenericList<T>
+    {
+        private class Node
+        {
+
+            private Node next;
+            public T data;
+
+            public Node(T t)
+            {
+                data = t;
+                next = null;
+            }
+
+            public Node Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+            public T Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+        }
+        Node head;
+        public MyGenericList()
+        {
+            head = null;
+        }
+
+        public void addHead(T t)
+        {
+            Node n = new Node(t);
+            n.Next = head;
+            head = n;
+            
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                yield return current.data;
+                current = current.Next;
+            }
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            // int is the type argument
+            MyGenericList<int> list = new MyGenericList<int>();
+            list.addHead(4);
+            list.addHead(5);
+            list.addHead(6);
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
         }
     }
 }
